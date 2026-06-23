3fc881f3d05531572c478db33d68ae0d90f97236
GonzagAlosius26 <johnkonslas@gmail.com>
2026-06-23 20:42:56 +0530

    Add CI/CD workflow for building and deploying WAR to Apache Tomcat; configure pom.xml for WAR packaging and create a sample REST controller

---
+++ src/main/java/HRMS/Backend/Controller/cont.java
@@ -0,0 +1,14 @@
+package HRMS.Backend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class cont {
+    @GetMapping("/b")
+    public String get() {
+        return "Hello";
+    }
+}
